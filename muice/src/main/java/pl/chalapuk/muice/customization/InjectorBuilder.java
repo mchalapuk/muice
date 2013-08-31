b4ea5f45710156ea4c43188e2c61b5f4edf627d6
Maciej Chałapuk <maciej@chalapuk.pl>
2013-08-31 22:27:40 +0200

    + JavaDoc for InjectorBuilder

--- muice/src/main/java/pl/chalapuk/muice/customization/InjectorBuilder.java
+++ muice/src/main/java/pl/chalapuk/muice/customization/InjectorBuilder.java
@@ -16,15 +16,61 @@
 
 package pl.chalapuk.muice.customization;
 
+import pl.chalapuk.muice.BindingError;
 import pl.chalapuk.muice.BindingModule;
 import pl.chalapuk.muice.Injector;
+import pl.chalapuk.muice.Muice;
 import pl.chalapuk.muice.internal.InjectorBuilderImpl;
 
+/**
+ * Builds one instance of {@link Injector}. Library client uses this interface
+ * to create injectors when using custom Muice instance.
+ * <p>
+ * Usage of the builder is divided in two stages:
+ * <ol>
+ * <li>processing binding modules (by calling {@link #withModules()} methods)
+ * <li>build finalization (by calling {@link #build()} method)
+ * </ol>
+ * 
+ * @see Muice#newInjector()
+ * @author maciej@chalapuk.pl (Maciej Chałapuk)
+ */
 public interface InjectorBuilder {
 
-    InjectorBuilderImpl withModules(BindingModule... modules);
+    /**
+     * Processes given binding modules.
+     * <p>
+     * Passed modules will be processed in the same order as passed.
+     * 
+     * @param modules binding modules to be processed
+     * @return {@code this}
+     * @throws IllegalStateException if build was already finalized
+     * @throws BindingError if binding configuration in passed modules contain
+     *             binding error
+     */
+    InjectorBuilderImpl withModules(BindingModule... modules)
+            throws IllegalStateException, BindingError;
 
-    InjectorBuilderImpl withModules(Iterable<BindingModule> modules);
+    /**
+     * Processes given binding modules.
+     * <p>
+     * Passed modules will be processed in the same order they are occur.
+     * 
+     * @param modules binding modules to be processed
+     * @return {@code this}
+     * @throws IllegalStateException if build was already finalized
+     * @throws BindingError if binding configuration in passed modules contain
+     */
+    InjectorBuilderImpl withModules(Iterable<BindingModule> modules)
+            throws IllegalStateException, BindingError;
 
-    Injector build();
+    /**
+     * Finalizes building and returns created injector.
+     * 
+     * @return new inejctor instance configured with bindings previously passed
+     *         to {@link #withModules()} methods.
+     * @throws IllegalStateException if build was already finalized
+     */
+    Injector build()
+            throws IllegalStateException;
 }
